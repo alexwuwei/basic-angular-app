66f4cc5ff9858d7526e12edb95aa3c7dfcd379b9
Hugo A Guerra <guerrahugoa@gmail.com>
2016-04-05 20:00:01 -0700

    created server.js, initialized webpack file, added css styling, initialized angular module, initialized node

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let express = require('express');
+let app = express();
+
+let publicRouter = express.Router();
+
+app.use('/', publicRouter);
+
+app.listen(3000, () => {
+  console.log('listening on 3000');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+});
